5d46a1f9eac654690bd2236ef295abe6b0d26cf7
Pk90500 <91583076+Pk90500@users.noreply.github.com>
2022-08-30 12:28:05 +0530

    update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -110,15 +110,20 @@ def stream_markup(_, videoid, chat_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["PL_B_2"],
-                callback_data=f"add_playlist {videoid}",
+                text="❰𝙊𝙬𝙣𝙚𝙧❱",
+                url=f"https://t.me/Army0071"
             ),
             InlineKeyboardButton(
-                text=_["PL_B_3"],
-                callback_data=f"PanelMarkup {videoid}|{chat_id}",
+                text="❰𝙂𝙧𝙤𝙪𝙥❱",
+                url=f"https://t.me/World_friends_chatting_zone"
             ),
+            ],
+        [
+            InlineKeyboardButton(
+                text="❰𝘿𝙊𝙉𝘼𝙏𝙀❱",
+                url=f"https://payu.in/web/B929AE5F10F28A6E751AA02D7458EDEA"
+            )
         ],
-        [InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data="close")],
     ]
     return buttons
 
